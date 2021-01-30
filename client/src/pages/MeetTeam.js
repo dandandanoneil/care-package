1d9a96d9e93f19b422f8a07052c6ebacdc9249e3
Skyler Dakota Lawton <skylerdakota@gmail.com>
2021-01-30 11:51:06 -0500

    added component folder, delete button component, pages folder, pages with boiler plate code, seeds file with updates needed to match model

---
+++ client/src/pages/MeetTeam.js
@@ -0,0 +1,16 @@
+// import React, { useEffect, useState } from "react";
+// import API from "../../utils/API";
+
+
+// function MeetTeam() {
+
+ 
+ 
+
+//     return (
+    
+//     );
+//   }
+
+
+// export default MeetTeam;
