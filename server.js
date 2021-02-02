ef138e92bf2b1bfd63fddc698ec57d8752f20e23
Dan ONeil <dhoneil@gmail.com>
2021-02-02 17:09:56 -0500

    Initial build of passport.js back end code

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('./config/passport');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
+
+// Configure passport
+app.use(express.static('public'));
+app.use(session({ secret: 'cats' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(PORT, function() {
