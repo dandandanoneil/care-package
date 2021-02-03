65baab443b519f9dbaef7e5339f7c293bc251839
Luis Canahuate <luis.canahuate16@gmail.com>
2021-02-02 22:13:08 -0500

    added dropdown to filter and updated buttons

--- client/src/components/Buttons/index.js
+++ client/src/components/Buttons/index.js
@@ -4,19 +4,19 @@ import Button from 'react-bootstrap/Button'
 
 const Buttons = () => {
     return (
-        <div className= 'd-flex justify-content-center home-button'>
+        <div className= 'mt-3 d-flex justify-content-center home-button'>
             
-  <div className="mb-2 mx-5"> 
-    <Button variant="light" size="lg" style={{
-         backgroundImage: `url("https://ps.w.org/woocommerce-gift-wrapper/assets/icon-256x256.png?rev=1301673")`,backgroundRepeat: 'no-repeat',width:'250px',height:'190px',color:'white'
+  <div className="mb-2 mx-3"> 
+    <Button variant="success" size="lg" style={{
+        //  backgroundImage: `url("https://ps.w.org/woocommerce-gift-wrapper/assets/icon-256x256.png?rev=1301673")`,backgroundRepeat: 'no-repeat',width:'250px',height:'190px',color:'white'
       }}>
       Give
     </Button>{' '}
     </div>
 
-    <div className="mb-2 mx-5">
-    <Button variant="light" size="lg" style={{
-         backgroundImage: `url("https://ps.w.org/woocommerce-gift-wrapper/assets/icon-256x256.png?rev=1301673")`,backgroundRepeat: 'no-repeat',width:'250px',height:'190px',color:'white'
+    <div className="mb-2 mx-3">
+    <Button variant="info" size="lg" style={{
+        //  backgroundImage: `url("https://ps.w.org/woocommerce-gift-wrapper/assets/icon-256x256.png?rev=1301673")`,backgroundRepeat: 'no-repeat',width:'250px',height:'190px',color:'white'
       }}>
       Receive
     </Button>
--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Dropdown from 'react-bootstrap/Dropdown'
+import Filter from '../Filter'
 
 const Search = () => {
     return (
         <div className= "d-flex justify-content-center">
-            <input placeholder="Search for a item"></input>
-            <Button>Search</Button>
+            <input placeholder="Search for a item"></input> &nbsp;&nbsp;
+            <Button>Search</Button>&nbsp;
             <Dropdown>
   <Dropdown.Toggle variant="success" id="dropdown-basic">
    Filter
   </Dropdown.Toggle>
 
   <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    <Filter />
+ 
   </Dropdown.Menu>
 </Dropdown>
+      
         </div>
     )
 }
