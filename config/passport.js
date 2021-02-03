b9412e8f7785238240765a0bd51b6e41f21c4ad3
Dan ONeil <dhoneil@gmail.com>
2021-02-03 17:32:24 -0500

    Fixed login and signup functions

--- config/passport.js
+++ config/passport.js
@@ -1,31 +1,24 @@
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-var db = require("../models");
+const db = require("../models");
 
-// Tell passport we want to use a Local Strategy
 passport.use(new LocalStrategy(
-  // Users will sign in using an email (rather than a username)
   { usernameField: "email" },
+  // This function handles a user trying to log in
   function(email, password, done) {
-    // When a user tries to sign in this code runs
-    db.User.findOne({
-      where: { email: email }
-    }).then(function(dbUser) {
+    db.User.findOne({ email: email }, function(err, user) {
+      // If there's an error
+      if (err) return done(err);
+
       // If the user doesn't exist
-      if (!dbUser) {
-        return done(null, false, {
-          message: "No account exists with this email."
-        });
-      }
+      if (!user) return done(null, false, { message: "No user exists with that email." });
+
       // If the password doesn't match
-      else if (!dbUser.validPassword(password)) {
-        return done(null, false, {
-          message: "Incorrect password."
-        });
-      }
+      if (!user.validPassword(password))  return done(null, false, { message: "Incorrect password." });
+
       // Otherwise, return the user and signal to let them log in
-      return done(null, dbUser);
+      return done(null, user);
     });
   }
 ));
